ccb2f00014dc55aba7b659d7e5bfa1f8c35184d7
tibbi <tibor@kaputa.sk>
2016-09-08 22:57:07 +0200

    convert ItemsAdapter to kotlin

---
+++ app/src/main/java/com/simplemobiletools/filemanager/adapters/ItemsAdapter.kt
@@ -0,0 +1,78 @@
+package com.simplemobiletools.filemanager.adapters
+
+import android.content.Context
+import android.content.res.Resources
+import android.graphics.Bitmap
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.simplemobiletools.filemanager.R
+import com.simplemobiletools.filemanager.Utils
+import com.simplemobiletools.filemanager.models.FileDirItem
+import kotlinx.android.synthetic.main.list_item.view.*
+
+class ItemsAdapter(context: Context, private val mItems: List<FileDirItem>) : BaseAdapter() {
+    private val mInflater: LayoutInflater
+    private val mFileBmp: Bitmap
+    private val mDirectoryBmp: Bitmap
+    private val mRes: Resources
+
+    init {
+        mInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+        mRes = context.resources
+        mDirectoryBmp = Utils.getColoredIcon(mRes, R.color.lightGrey, R.mipmap.directory)
+        mFileBmp = Utils.getColoredIcon(mRes, R.color.lightGrey, R.mipmap.file)
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var view = convertView
+        val viewHolder: ViewHolder
+        if (view == null) {
+            view = mInflater.inflate(R.layout.list_item, parent, false)
+            viewHolder = ViewHolder(view)
+            view!!.tag = viewHolder
+        } else {
+            viewHolder = view.tag as ViewHolder
+        }
+
+        val item = mItems[position]
+        viewHolder.name.text = item.name
+
+        if (item.isDirectory) {
+            viewHolder.icon.setImageBitmap(mDirectoryBmp)
+            viewHolder.details.text = getChildrenCnt(item)
+        } else {
+            viewHolder.icon.setImageBitmap(mFileBmp)
+            viewHolder.details.text = Utils.getFormattedSize(item)
+        }
+
+        return view
+    }
+
+    private fun getChildrenCnt(item: FileDirItem): String {
+        val children = item.children
+        return mRes.getQuantityString(R.plurals.items, children, children)
+    }
+
+    override fun getCount(): Int {
+        return mItems.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return mItems[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return 0
+    }
+
+    internal class ViewHolder(view: View) {
+        val name: TextView = view.item_name
+        val icon: ImageView = view.item_icon
+        val details: TextView = view.item_details
+    }
+}
