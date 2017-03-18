9327542b7a7fa735bceda3e1fcbff53a074098a5
tibbi <tibor@kaputa.sk>
2017-03-18 18:41:14 +0100

    list favorite folders in the Manage favorites activity

--- app/src/main/kotlin/com/simplemobiletools/filemanager/activities/FavoritesActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/filemanager/activities/FavoritesActivity.kt
@@ -1,11 +1,38 @@
 package com.simplemobiletools.filemanager.activities
 
+import android.graphics.PorterDuff
 import android.os.Bundle
 import com.simplemobiletools.filemanager.R
+import com.simplemobiletools.filemanager.extensions.config
+import kotlinx.android.synthetic.main.activity_favorites.*
+import kotlinx.android.synthetic.main.item_favorite.view.*
 
 class FavoritesActivity : SimpleActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favorites)
+        updateFavorites()
+    }
+
+    private fun updateFavorites() {
+        favorites_holder.removeAllViews()
+        val folders = config.favorites
+
+        for (folder in folders) {
+            layoutInflater.inflate(R.layout.item_favorite, null, false).apply {
+                favorite_title.apply {
+                    text = folder
+                    setTextColor(config.textColor)
+                }
+                favorite_icon.apply {
+                    setColorFilter(config.textColor, PorterDuff.Mode.SRC_IN)
+                    setOnClickListener {
+                        config.removeFavorite(folder)
+                        updateFavorites()
+                    }
+                }
+                favorites_holder.addView(this)
+            }
+        }
     }
 }
