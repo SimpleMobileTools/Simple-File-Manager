80d834e7eb1aa354e2e890c25ef48353c377403e
tibbi <tibor@kaputa.sk>
2017-09-03 18:18:03 +0200

    add a helper function for fetching root items

---
+++ app/src/main/kotlin/com/simplemobiletools/filemanager/helpers/RootHelpers.kt
@@ -0,0 +1,33 @@
+package com.simplemobiletools.filemanager.helpers
+
+import com.simplemobiletools.commons.models.FileDirItem
+import com.stericson.RootShell.execution.Command
+import com.stericson.RootTools.RootTools
+
+class RootHelpers {
+    fun getFiles(path: String, callback: (fileDirItems: ArrayList<FileDirItem>) -> Unit) {
+        val command = object : Command(0, "ls -la $path | awk '{print \$1,\$NF}'") {
+            override fun commandOutput(id: Int, line: String) {
+                super.commandOutput(id, line)
+                val parts = line.split(" ")
+
+                val files = ArrayList<FileDirItem>()
+                val filename = parts[1]
+                val filePath = "${path.trimEnd('/')}/$filename"
+                val isDirectory = parts[0].startsWith("d")
+                val fileDirItem = FileDirItem(filePath, filename, isDirectory, 0, 0)
+                files.add(fileDirItem)
+                callback(files)
+            }
+
+            override fun commandTerminated(id: Int, reason: String?) {
+                super.commandTerminated(id, reason)
+            }
+
+            override fun commandCompleted(id: Int, exitcode: Int) {
+                super.commandCompleted(id, exitcode)
+            }
+        }
+        RootTools.getShell(true).add(command)
+    }
+}
