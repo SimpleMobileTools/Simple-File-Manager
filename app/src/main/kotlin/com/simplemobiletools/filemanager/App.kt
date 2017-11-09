27da8b48412e1882124e6ce0d1c1555636e74b38
tibbi <tibor@kaputa.sk>
2017-11-09 20:56:53 +0100

    adding LeakCanary

--- app/src/main/kotlin/com/simplemobiletools/filemanager/App.kt
+++ app/src/main/kotlin/com/simplemobiletools/filemanager/App.kt
@@ -2,12 +2,20 @@ package com.simplemobiletools.filemanager
 
 import android.app.Application
 import com.github.ajalt.reprint.core.Reprint
+import com.simplemobiletools.filemanager.BuildConfig.USE_LEAK_CANARY
 import com.simplemobiletools.filemanager.extensions.config
+import com.squareup.leakcanary.LeakCanary
 import java.util.*
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+        if (USE_LEAK_CANARY) {
+            if (LeakCanary.isInAnalyzerProcess(this)) {
+                return
+            }
+            LeakCanary.install(this)
+        }
 
         if (config.useEnglish) {
             val conf = resources.configuration
