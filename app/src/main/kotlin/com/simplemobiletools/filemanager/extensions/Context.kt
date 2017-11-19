18104b98c02d6144e6cfb0fce03245c54a681ea7
tibbi <tibor@kaputa.sk>
2017-11-19 18:39:36 +0100

    capitalize Context and String extensions

--- app/src/main/kotlin/com/simplemobiletools/filemanager/extensions/Context.kt
+++ app/src/main/kotlin/com/simplemobiletools/filemanager/extensions/Context.kt
@@ -4,6 +4,6 @@ import android.content.Context
 import com.simplemobiletools.commons.extensions.hasExternalSDCard
 import com.simplemobiletools.filemanager.helpers.Config
 
-val Context.config: Config get() = Config.newInstance(this)
+val Context.config: Config get() = Config.newInstance(applicationContext)
 
 fun Context.isPathOnRoot(path: String) = !(path.startsWith(config.internalStoragePath) || (hasExternalSDCard() && path.startsWith(config.sdCardPath)))
