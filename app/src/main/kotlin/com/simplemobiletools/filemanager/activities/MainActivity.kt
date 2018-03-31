ce466645c5f5ed7d87f4bb83ace109bde12f29ea
tibbi <tibor@kaputa.sk>
2018-03-31 14:38:21 +0200

    flip FAQ items

--- app/src/main/kotlin/com/simplemobiletools/filemanager/activities/MainActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/filemanager/activities/MainActivity.kt
@@ -248,8 +248,8 @@ class MainActivity : SimpleActivity() {
 
     private fun launchAbout() {
         val faqItems = arrayListOf(
-                FAQItem(R.string.faq_2_title_commons, R.string.faq_2_text_commons),
-                FAQItem(R.string.faq_3_title_commons, R.string.faq_3_text_commons))
+                FAQItem(R.string.faq_3_title_commons, R.string.faq_3_text_commons),
+                FAQItem(R.string.faq_2_title_commons, R.string.faq_2_text_commons))
 
         startAboutActivity(R.string.app_name, LICENSE_MULTISELECT or LICENSE_GLIDE or LICENSE_PATTERN or LICENSE_REPRINT,
                 BuildConfig.VERSION_NAME, faqItems)
