d7fd4fb0d17b9488db098127bc1d0fc76c2e90b4
tibbi <tibor@kaputa.sk>
2018-08-07 14:29:06 +0200

    update commons to 4.6.0

--- app/src/main/kotlin/com/simplemobiletools/filemanager/activities/MainActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/filemanager/activities/MainActivity.kt
@@ -247,12 +247,14 @@ class MainActivity : SimpleActivity() {
     }
 
     private fun launchAbout() {
+        val licenses = LICENSE_MULTISELECT or LICENSE_GLIDE or LICENSE_PATTERN or LICENSE_REPRINT
+
         val faqItems = arrayListOf(
                 FAQItem(R.string.faq_3_title_commons, R.string.faq_3_text_commons),
                 FAQItem(R.string.faq_2_title_commons, R.string.faq_2_text_commons)
         )
 
-        startAboutActivity(R.string.app_name, LICENSE_MULTISELECT or LICENSE_GLIDE or LICENSE_PATTERN or LICENSE_REPRINT, BuildConfig.VERSION_NAME, faqItems)
+        startAboutActivity(R.string.app_name, licenses, BuildConfig.VERSION_NAME, faqItems, true)
     }
 
     override fun onBackPressed() {
