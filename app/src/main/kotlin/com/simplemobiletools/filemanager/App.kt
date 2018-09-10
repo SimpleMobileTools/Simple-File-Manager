3ee6bbe911b8447f97ba5827970dd928caba3a59
tibbi <tibor@kaputa.sk>
2018-09-10 17:51:26 +0200

    updating kotlin, gradle, commons, removing LeakCanary

--- app/src/main/kotlin/com/simplemobiletools/filemanager/App.kt
+++ app/src/main/kotlin/com/simplemobiletools/filemanager/App.kt
@@ -3,18 +3,10 @@ package com.simplemobiletools.filemanager
 import android.app.Application
 import com.github.ajalt.reprint.core.Reprint
 import com.simplemobiletools.commons.extensions.checkUseEnglish
-import com.squareup.leakcanary.LeakCanary
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-        if (BuildConfig.DEBUG) {
-            if (LeakCanary.isInAnalyzerProcess(this)) {
-                return
-            }
-            LeakCanary.install(this)
-        }
-
         checkUseEnglish()
         Reprint.initialize(this)
     }
