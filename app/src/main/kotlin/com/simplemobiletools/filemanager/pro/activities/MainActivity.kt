9b00f605437a8cc4de0bd1e208730a640f7a6043
tibbi <tibor@kaputa.sk>
2019-08-28 15:56:17 +0200

    adding a new FAQ item related to download issues after a payment

--- app/src/main/kotlin/com/simplemobiletools/filemanager/pro/activities/MainActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/filemanager/pro/activities/MainActivity.kt
@@ -319,7 +319,8 @@ class MainActivity : SimpleActivity() {
         val faqItems = arrayListOf(
                 FAQItem(R.string.faq_3_title_commons, R.string.faq_3_text_commons),
                 FAQItem(R.string.faq_2_title_commons, R.string.faq_2_text_commons),
-                FAQItem(R.string.faq_6_title_commons, R.string.faq_6_text_commons)
+                FAQItem(R.string.faq_6_title_commons, R.string.faq_6_text_commons),
+                FAQItem(R.string.faq_7_title_commons, R.string.faq_7_text_commons)
         )
 
         startAboutActivity(R.string.app_name, licenses, BuildConfig.VERSION_NAME, faqItems, true)
