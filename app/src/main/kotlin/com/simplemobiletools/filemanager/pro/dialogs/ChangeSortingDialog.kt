e665c38f05cb607d613616bc5a873c0d67c75f56
tibbi <tibor@kaputa.sk>
2020-06-07 22:50:39 +0200

    allow sorting items by proper numeric values in their names

--- app/src/main/kotlin/com/simplemobiletools/filemanager/pro/dialogs/ChangeSortingDialog.kt
+++ app/src/main/kotlin/com/simplemobiletools/filemanager/pro/dialogs/ChangeSortingDialog.kt
@@ -1,7 +1,9 @@
 package com.simplemobiletools.filemanager.pro.dialogs
 
+import android.view.View
 import androidx.appcompat.app.AlertDialog
 import com.simplemobiletools.commons.activities.BaseSimpleActivity
+import com.simplemobiletools.commons.extensions.beVisibleIf
 import com.simplemobiletools.commons.extensions.setupDialogStuff
 import com.simplemobiletools.commons.helpers.*
 import com.simplemobiletools.filemanager.pro.R
@@ -11,25 +13,36 @@ import kotlinx.android.synthetic.main.dialog_change_sorting.view.*
 class ChangeSortingDialog(val activity: BaseSimpleActivity, val path: String = "", val callback: () -> Unit) {
     private var currSorting = 0
     private var config = activity.config
-    private var view = activity.layoutInflater.inflate(R.layout.dialog_change_sorting, null)
+    private var view: View
 
     init {
-        view.sorting_dialog_use_for_this_folder.isChecked = config.hasCustomSorting(path)
+        currSorting = config.getFolderSorting(path)
+        view = activity.layoutInflater.inflate(R.layout.dialog_change_sorting, null).apply {
+            sorting_dialog_use_for_this_folder.isChecked = config.hasCustomSorting(path)
+
+            sorting_dialog_numeric_sorting.beVisibleIf(currSorting and SORT_BY_NAME != 0)
+            sorting_dialog_numeric_sorting.isChecked = currSorting and SORT_USE_NUMERIC_VALUE != 0
+        }
 
         AlertDialog.Builder(activity)
-                .setPositiveButton(R.string.ok) { dialog, which -> dialogConfirmed() }
-                .setNegativeButton(R.string.cancel, null)
-                .create().apply {
-                    activity.setupDialogStuff(view, this, R.string.sort_by)
-                }
+            .setPositiveButton(R.string.ok) { dialog, which -> dialogConfirmed() }
+            .setNegativeButton(R.string.cancel, null)
+            .create().apply {
+                activity.setupDialogStuff(view, this, R.string.sort_by)
+            }
 
-        currSorting = config.getFolderSorting(path)
         setupSortRadio()
         setupOrderRadio()
     }
 
     private fun setupSortRadio() {
         val sortingRadio = view.sorting_dialog_radio_sorting
+
+        sortingRadio.setOnCheckedChangeListener { group, checkedId ->
+            val isSortingByName = checkedId == sortingRadio.sorting_dialog_radio_name.id
+            view.sorting_dialog_numeric_sorting.beVisibleIf(isSortingByName)
+        }
+
         val sortBtn = when {
             currSorting and SORT_BY_SIZE != 0 -> sortingRadio.sorting_dialog_radio_size
             currSorting and SORT_BY_DATE_MODIFIED != 0 -> sortingRadio.sorting_dialog_radio_last_modified
@@ -60,6 +73,10 @@ class ChangeSortingDialog(val activity: BaseSimpleActivity, val path: String = "
 
         if (view.sorting_dialog_radio_order.checkedRadioButtonId == R.id.sorting_dialog_radio_descending) {
             sorting = sorting or SORT_DESCENDING
+        }
+
+        if (view.sorting_dialog_numeric_sorting.isChecked) {
+            sorting = sorting or SORT_USE_NUMERIC_VALUE
         }
 
         if (view.sorting_dialog_use_for_this_folder.isChecked) {
